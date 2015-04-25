6455d12de86c7da675bf5522bf270b2049b60e62
erich brunner <erich.brunner@live.at>
2015-04-25 05:06:45 +0200

    added chapter 2.1

---
+++ TaskDelayAndRetry.Tests/TaskDelayAndRetryTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDelayAndRetry.Tests
+{
+    /// <summary>
+    /// Tests for class <see cref="TaskDelayAndRetry"/>
+    /// </summary>
+    [TestFixture]
+    class TaskDelayAndRetryTests
+    {
+        /// <summary>
+        /// Tests that a delayed result is yielded back asynchronously.
+        /// </summary>
+        [Test]
+        public async void DelayResult_TimeSpanSet_ReturnsDelayedResult()
+        {
+            // ARRANGE
+            TimeSpan timespan = TimeSpan.FromSeconds(2);
+            const int ExpectedResult = 100;
+
+            // ACT
+            var result = await TaskDelayAndRetry.DelayResult(result: ExpectedResult, delay: timespan);
+            
+            // ASSERT
+            Assert.AreEqual(ExpectedResult, result);
+        }
+        
+    }
+}
---
+++ TaskDelayAndRetry/TaskDelayAndRetry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDelayAndRetry
+{
+    public class TaskDelayAndRetry
+    {
+        public static async Task<T> DelayResult<T>(T result, TimeSpan delay)
+        {
+            await Task.Delay(delay);
+            return result;
+        }
+
+
+    }
+}
