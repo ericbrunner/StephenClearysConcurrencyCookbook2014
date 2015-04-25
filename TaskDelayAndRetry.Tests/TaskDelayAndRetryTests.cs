34cff68aa7803bacd9b56d5b98e874c439c5af1c
erich brunner <erich.brunner@live.at>
2015-04-25 06:14:49 +0200

    completed chapter 2.1

--- TaskDelayAndRetry.Tests/TaskDelayAndRetryTests.cs
+++ TaskDelayAndRetry.Tests/TaskDelayAndRetryTests.cs
@@ -29,6 +29,70 @@ namespace TaskDelayAndRetry.Tests
             // ASSERT
             Assert.AreEqual(ExpectedResult, result);
         }
+
+        /// <summary>
+        /// Tests that the html uri string gets asynchronously returned by the method under
+        /// test.
+        /// 
+        /// NOTE: To test the timeout you have to fake the httpclient with MSFakes to 
+        /// establish the same result if fakes supports await-able methods.
+        /// </summary>
+        [Test]
+        public async void DownloadStringWithTimeout_UriSetNoTimeOut_ReturnsFetchedUriString()
+        {
+            // ARRANGE
+            // - nothing to arrange
+
+            // ACT
+            var result = await TaskDelayAndRetry.DownloadStringWithTimeout(uri: "http://stephencleary.com/");
+            
+            // ASSERT
+            Assert.NotNull(result, "result is null.");
+            
+            var index = result.IndexOf(
+                "http://stephencleary.com", 
+                0, 
+                result.Length, 
+                StringComparison.OrdinalIgnoreCase);
+
+            Assert.AreNotEqual(-1, index, "index has unexpected value.");
+        }
+
+        /// <summary>
+        /// Downloads the string with retries.
+        /// 
+        /// Note: To test the timeout simply disconnect from your network and debug the test.
+        /// You can fake the httpclient to with MSFakes to establish the same result if fakes
+        /// supports await-able methods.
+        /// 
+        /// In the Output Window you should see something like this:
+        /// 
+        /// Exception occured at Retry Fetch Attempt '1'.
+        /// Message: An error occurred while sending the request.
+        /// A first chance exception of type 'System.Net.Http.HttpRequestException' occurred in mscorlib.dll
+        /// Exception occured at Retry Fetch Attempt '2'.
+        /// Message: An error occurred while sending the request.
+        /// </summary>
+        [Test]
+        public async void DownloadStringWithRetries_UriSet_ReturnsFetchedUriString()
+        {
+            // ARRANGE
+            // - nothing to arrange
+
+            // ACT
+            var result = await TaskDelayAndRetry.DownloadStringWithRetries(uri: "http://stephencleary.com/");
+
+            // ASSERT
+            Assert.NotNull(result, "result is null.");
+
+            var index = result.IndexOf(
+                "http://stephencleary.com",
+                0,
+                result.Length,
+                StringComparison.OrdinalIgnoreCase);
+
+            Assert.AreNotEqual(-1, index, "index has unexpected value.");
+        }
         
     }
 }
--- TaskDelayAndRetry/TaskDelayAndRetry.cs
+++ TaskDelayAndRetry/TaskDelayAndRetry.cs
@@ -3,17 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TaskDelayAndRetry
 {
-    public class TaskDelayAndRetry
+    /// <summary>
+    /// That class covers chapter 2.1 "Pausing for a Period of Time.
+    /// </summary>
+    public static class TaskDelayAndRetry
     {
+        /// <summary>
+        /// Delays the result.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result">The result.</param>
+        /// <param name="delay">The delay.</param>
+        /// <returns></returns>
         public static async Task<T> DelayResult<T>(T result, TimeSpan delay)
         {
             await Task.Delay(delay);
             return result;
         }
 
+        /// <summary>
+        /// Downloads the string with retries.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns></returns>
+        public static async Task<string> DownloadStringWithRetries(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                // Retry after 1 second, then after 2 seconds, the 4 (math power of 2: n^2)
+                var nextDelay = TimeSpan.FromSeconds(1);
 
+                for (int i = 0; i != 3; ++i)
+                {
+                    try
+                    {
+                        return await client.GetStringAsync(uri);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Diagnostics.Debug.WriteLine(
+                            string.Format("Exception occured at Retry Fetch Attempt '{0}'." +
+                            Environment.NewLine +
+                            "Message: {1}",
+                            i,
+                            e.Message));
+                        
+                    }
+
+
+                    await Task.Delay(nextDelay);
+                    nextDelay = nextDelay + nextDelay;
+                }
+
+                // Retry one last time, allowing the error to propagate.
+                return await client.GetStringAsync(uri);
+            }
+        }
+
+        /// <summary>
+        /// Downloads the string with timeout.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns></returns>
+        public static async Task<string> DownloadStringWithTimeout(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                var downloadTask = client.GetStringAsync(uri);
+                var timeoutTask = Task.Delay(3000);
+
+                var completedTask = await Task.WhenAny(downloadTask, timeoutTask);
+
+                if (completedTask == timeoutTask)
+                {
+                    return null;
+                }
+
+                return await downloadTask;
+            }
+        }
     }
 }
