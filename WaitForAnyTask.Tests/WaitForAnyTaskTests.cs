dee5e3c15b6db8a9c5175468bede1235612fd810
erich brunner <erich.brunner@live.at>
2015-04-26 12:58:55 +0200

    added chapter 2.5 Waiting for Any Task to Complete

---
+++ WaitForAnyTask.Tests/WaitForAnyTaskTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WaitForAnyTask.Tests
+{
+    /// <summary>
+    /// Tests class <see cref="WaitForAnyTask"/>.
+    /// </summary>
+    [TestFixture]
+    public class WaitForAnyTaskTests
+    {
+        /// <summary>
+        /// Tests that the downloaded html data length from the first completed task is retured.
+        /// NOTE: Run test several times to see in the Output Window something like that:
+        /// 
+        /// Completed Task is: 'downloadTaskA'
+        /// 
+        /// next test run ...
+        /// Completed Task is: 'downloadTaskB'
+        /// 
+        /// </summary>
+        [Test]
+        public async void FirstRespondingUrlAsync_TwoEqualURLs_ReturnsDataLengthOfFirstCompletedTask()
+        {
+            // ARRANGE
+            const string urlA = "https://msdn.microsoft.com", urlB = "https://msdn.microsoft.com";
+
+            // ACT
+            var resultTask = WaitForAnyTask.FirstRespondingUrlAsync(urlA, urlB);
+
+            var result = await resultTask;
+
+            System.Diagnostics.Debug.WriteLine(
+                string.Format("Task completed in Status: '{0}'", resultTask.Status));
+
+            // ASSERT
+            Assert.AreNotEqual(default(int), result, "result has unexpected value.");
+        }
+    }
+}
---
+++ WaitForAnyTask/WaitForAnyTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaitForAnyTask
+{
+    public static class WaitForAnyTask
+    {
+
+        /// <summary>
+        /// Returns the data length of the first completed task.
+        /// </summary>
+        /// <param name="urlA">The URL a.</param>
+        /// <param name="urlB">The URL b.</param>
+        /// <returns></returns>
+        public static async Task<int> FirstRespondingUrlAsync(string urlA, string urlB)
+        {
+            var httpClient = new HttpClient();
+
+            // Start both downloads concurrently
+            Task<byte[]> downloadTaskA = httpClient.GetByteArrayAsync(urlA);
+            Task<byte[]> downloadTaskB = httpClient.GetByteArrayAsync(urlB);
+
+            // Wait for either of the tasks to complete.
+            Task<byte[]> completedTask = await Task.WhenAny(downloadTaskA, downloadTaskB);
+
+            System.Diagnostics.Debug.WriteLine(
+                string.Format("Completed Task is: '{0}'",
+                    (completedTask == downloadTaskA) ? "downloadTaskA" : "downloadTaskB"));
+
+            // Await the returned Task
+            byte[] data = await completedTask;
+
+            // Return the data length
+            return data.Length;
+        }
+    }
+}
