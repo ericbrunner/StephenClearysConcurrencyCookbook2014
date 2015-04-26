34445a51eb53eeed7814abeec908ce4e3d7dfa15
erich brunner <erich.brunner@live.at>
2015-04-26 08:27:12 +0200

    added chapter 2.3 "Reporting Progress"

---
+++ ReportingProgress.Tests/ReportingProgressTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+
+namespace ReportingProgress.Tests
+{
+    /// <summary>
+    /// Tests class <see cref="ReportingProgress"/>
+    /// </summary>
+    [TestFixture]
+    public class ReportingProgressTests
+    {
+        /// <summary>
+        /// Tests that the method under test fired the progress event 10 times.
+        /// </summary>
+        [Test]
+        public async void MyMethodAsync_ProgressHandlerSet_ProgressEventFired()
+        {
+            // ARRANGE
+            const double ExpectedPercentage = 100;
+            double percentageComplete = 0;
+            var progress = new Progress<double>();
+            progress.ProgressChanged += (sender, percentage) => 
+            {
+                percentageComplete = percentage;
+                System.Diagnostics.Debug.WriteLine("Complete Percentage '{0}%'", percentageComplete);
+            };
+
+            // ACT
+            await ReportingProgress.MyMethodAsync(progress);
+
+            /*
+             * If you run the test and look in the Output window 
+             * the Percentage is 100% but the test fails.
+             * 
+             * Stephen Cleary mentioned in the discussion section the asynchronously
+             * reporting behaviour. 
+             * 
+             * If the Task returned by MyMethodAsync has completed, the unit test
+             * continous with the next operation which is the Assert.AreEquals(...)
+             * below in the calling code of the unit test.
+             * 
+             * At that time the 'percentageComplete' variable may have not been
+             * updated from the asynchrously invoked progress eventhandler.
+             * 
+             * If you uncomment the Task.Delay below it should be enought time to get 
+             * the 'percentageComplete' to be updated in the eventhandler and the
+             * Assert should pass.
+             *  
+             * */
+            
+            await Task.Delay(100);
+
+            // ASSERT
+            Assert.AreEqual(ExpectedPercentage, percentageComplete, "percentageComplete has unexpected value.");
+        }
+    }
+}
---
+++ ReportingProgress/ReportingProgress.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingProgress
+{
+    /// <summary>
+    /// Tests the IProghress<typeparamref name="T"/> behaviour of an asynchronous method.
+    /// </summary>
+    public class ReportingProgress
+    {
+        /// <summary>
+        /// An await-able asynchronous method reporting progress if formal
+        /// argument progress set to some instance implementing IProgress<typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="progress">The progress.</param>
+        /// <returns></returns>
+        public static async Task MyMethodAsync(IProgress<double> progress = null)
+        {
+            double percentComplete = 0;
+            bool done = false;
+
+            while (!done)
+            {
+                // simulate some asynchronous work (e.g. File I/O) ...
+                await Task.Delay(100);
+
+                percentComplete += 10;
+
+                if (progress != null)
+                {
+                    progress.Report(percentComplete);
+                }
+
+                // exit the loop ...
+                if (percentComplete == 100)
+                {
+                    done = true;
+                }
+            }
+        }
+    }
+}
