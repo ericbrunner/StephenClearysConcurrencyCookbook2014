512ff1893a3d18bb3445ce0b25fe095506a55bda
erich brunner <erich.brunner@live.at>
2015-05-01 11:47:23 +0200

    added 2.7 Avoid Context on Continuations

---
+++ AvoidContextOnContinuation.Tests/AvoidContextOnContinuationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace AvoidContextOnContinuation.Tests
+{
+    [TestFixture]
+    public class AvoidContextOnContinuationTests
+    {
+        [Test]
+        public async void ResumeOnContextAsync()
+        {
+            // ARRANGE + ACT
+            var continuedOnSameContext = await AvoidContextOnContinuation.ResumeOnContextAsync();
+
+            // ASSERT
+            Assert.True(continuedOnSameContext, "continuedOnSameContext has unexpected value.");
+        }
+
+        [Test]
+        public async void ResumeWhitoutContextAsync()
+        {
+            // ARRANGE + ACT
+            var notContinuedOnSameContext = await AvoidContextOnContinuation.ResumeWhitoutContextAsync();
+
+            // ASSERT
+            Assert.True(notContinuedOnSameContext, "notContinuedOnSameContext has unexpected value.");
+        }
+    }
+}
---
+++ AvoidContextOnContinuation/AvoidContextOnContinuation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvoidContextOnContinuation
+{
+    public class AvoidContextOnContinuation
+    {
+        public static async Task<bool> ResumeOnContextAsync()
+        {
+            var synchronizationContextBefore = SynchronizationContext.Current;
+            var managedThreadIdAfterBefore = Thread.CurrentThread.ManagedThreadId;
+
+            System.Diagnostics.Debug.WriteLine(
+                string.Format(
+                "SynchronizationContext: {0}" +
+                Environment.NewLine +
+                "ManagedThreadId: {1}",
+                synchronizationContextBefore,
+                managedThreadIdAfterBefore));
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            // This method resumes within the same context
+
+            var synchronizationContextAfter = SynchronizationContext.Current;
+            var managedThreadIdAfter = Thread.CurrentThread.ManagedThreadId;
+
+            System.Diagnostics.Debug.WriteLine(
+                string.Format(
+                "SynchronizationContext: {0}" +
+                Environment.NewLine +
+                "ManagedThreadId: {1}",
+                (synchronizationContextAfter != null) ?
+                synchronizationContextAfter.ToString() : "is null",
+                managedThreadIdAfter));
+
+            return (synchronizationContextBefore.Equals(synchronizationContextAfter)) &&
+                   (managedThreadIdAfterBefore == managedThreadIdAfter);
+        }
+
+        public static async Task<bool> ResumeWhitoutContextAsync()
+        {
+            var synchronizationContextBefore = SynchronizationContext.Current;
+            var managedThreadIdAfterBefore = Thread.CurrentThread.ManagedThreadId;
+
+            System.Diagnostics.Debug.WriteLine(
+                string.Format(
+                "SynchronizationContext: {0}" +
+                Environment.NewLine +
+                "ManagedThreadId: {1}",
+                synchronizationContextBefore,
+                managedThreadIdAfterBefore));
+
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(continueOnCapturedContext: false);
+
+            // This method discards the context when it resumes
+
+            var synchronizationContextAfter = SynchronizationContext.Current;
+            var managedThreadIdAfter = Thread.CurrentThread.ManagedThreadId;
+            
+            System.Diagnostics.Debug.WriteLine(
+                string.Format(
+                "SynchronizationContext: {0}" +
+                Environment.NewLine +
+                "ManagedThreadId: {1}",
+                (synchronizationContextAfter != null) ?
+                synchronizationContextAfter.ToString() : "is null",
+                managedThreadIdAfter));
+
+            
+            return (!synchronizationContextBefore.Equals(synchronizationContextAfter)) && 
+                   (managedThreadIdAfterBefore != managedThreadIdAfter);
+        }
+    }
+}
