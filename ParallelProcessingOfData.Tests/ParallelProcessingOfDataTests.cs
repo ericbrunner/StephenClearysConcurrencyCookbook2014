ce47096c0c4eba9feda02faad9d9863157a68c97
Brunner Erich <Erich.Brunner@admiral.at>
2015-05-07 15:05:20 +0200

    added 3.1 Parallel Processing of Data 2/4

--- ParallelProcessingOfData.Tests/ParallelProcessingOfDataTests.cs
+++ ParallelProcessingOfData.Tests/ParallelProcessingOfDataTests.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using NUnit.Framework;
 using ParallelProcessingOfData.Interfaces;
 
 namespace ParallelProcessingOfData.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class ParallelProcessingOfDataTests
     {
-        [TestMethod]
+        [Test]
         public void RotateMatrices_AddedSomeMatrices_ReturnsLoopResult()
         {
             // ARRANGE
@@ -27,7 +27,30 @@ namespace ParallelProcessingOfData.Tests
             ParallelLoopResult result = ParallelProcessingOfData.RotateMatrices(matrices, 45f);
 
             // ASSERT
-            Assert.IsTrue(result.IsCompleted, "result.IsCompleted has unexpected value.");
+            Assert.True(result.IsCompleted, "result.IsCompleted has unexpected value.");
+        }
+
+
+        [Test]
+        public void InvertMatrices_AddedSomeMatrices_ReturnsLoopResult()
+        {
+            // ARRANGE
+            var mockMatrix = new Mock<IMatrix>();
+
+            mockMatrix.Setup(context => context.IsInvertible).Returns(false);
+            
+            List<IMatrix> matrices = new List<IMatrix>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                matrices.Add(mockMatrix.Object);
+            }
+
+            // ACT
+            ParallelLoopResult result = ParallelProcessingOfData.InvertMatrices(matrices);
+
+            // ASSERT
+            Assert.False(result.IsCompleted, "result.IsCompleted has unexpected value.");
         }
     }
 }
--- ParallelProcessingOfData/ParallelProcessingOfData.cs
+++ ParallelProcessingOfData/ParallelProcessingOfData.cs
@@ -10,6 +10,12 @@ namespace ParallelProcessingOfData
 {
     public class ParallelProcessingOfData
     {
+        /// <summary>
+        /// Rotates the matrices.
+        /// </summary>
+        /// <param name="matrices">The matrices.</param>
+        /// <param name="degree">The degree.</param>
+        /// <returns></returns>
         public static ParallelLoopResult RotateMatrices(IEnumerable<IMatrix> matrices, float degree)
         {
             ParallelLoopResult parallelLoopResult = 
@@ -18,11 +24,43 @@ namespace ParallelProcessingOfData
                     {
                         // simulate a bit of CPU-Bound processing time
                         matrix.Rotate(degree);
-                        
+
                         System.Diagnostics.Debug.WriteLine(
                             string.Format(
                             "Parallel Iteration: '{0}' is running on Thread: '{1}' " + 
                             Environment.NewLine + 
+                            "ParallelLoopState '{2}'", Thread.CurrentThread.ManagedThreadId, index, state));
+                    });
+
+            return parallelLoopResult;
+        }
+
+        /// <summary>
+        /// Inverts the matrices.
+        /// </summary>
+        /// <param name="matrices">The matrices.</param>
+        /// <returns></returns>
+        public static ParallelLoopResult InvertMatrices(IEnumerable<IMatrix> matrices)
+        {
+            ParallelLoopResult parallelLoopResult =
+                Parallel.ForEach(matrices,
+                    (matrix, state, index) =>
+                    {
+                        // simulate a bit of CPU-Bound processing time
+
+                        if (!matrix.IsInvertible)
+                        {
+                            state.Stop();
+                        }
+                        else
+                        {
+                            matrix.Invert();
+                        }
+
+                        System.Diagnostics.Debug.WriteLine(
+                            string.Format(
+                            "Parallel Iteration: '{0}' is running on Thread: '{1}' " +
+                            Environment.NewLine +
                             "ParallelLoopState '{2}'", Thread.CurrentThread.ManagedThreadId, index, state));
                     });
 
